9fc03e547413fe5af488641b479d6787d4d346d0
NICK B <nickbean2255@yahoo.com>
2024-07-20 04:05:44 -0400

    update

--- placeholder.py
+++ placeholder.py
@@ -1,6 +1,55 @@
 
 import os
 from time import sleep
+
+import subprocess
+
+
+def run(cmd: str, args: list = None, cwd: str = None, wait: bool = True, exe = None) -> str:
+    ''' 
+        to exec a command in the terminal
+        >>> --------------------------------------------------------------------
+        cmd: (str) = 'sudo apt-get upgrade'
+        args: (list) = ['Y']
+        cwd: (str) = '~/XoX'
+        wait: (bool) = False
+        >>> --------------------------------------------------------------------
+        try: run('apt-get upgrade', ['Y'], wait = False)
+        >>> -------------------------------------------------------------------- 
+        return str || None || raise ValueError
+    '''
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ excute the give code
+    terminal = subprocess.Popen(
+        cmd, 
+        shell = True,
+        stdin = subprocess.PIPE,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        universal_newlines = True,
+        cwd = cwd,
+        executable = exe,
+    )
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ return output
+    try:
+        if args: 
+                out = terminal.communicate(os.linesep.join(args))[0].strip()
+                if out[1] != '': 
+                    return out[1].strip()
+                else: 
+                    return out[0].strip()
+        elif wait: 
+            out = terminal.communicate()
+            if out[1] != '': 
+                return out[1].strip()
+            else: 
+                return out[0].strip()
+    except: pass
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ return output
+    return None
+
+out = run('git pull')
+print(out)
+
 os.system('./start.sh')
 
 x = 0
